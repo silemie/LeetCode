68193e936904116628935ccfc98c0d91fc1568f1
sileyang <yangsile123@gmail.com>
2019-06-13 10:11:15 +0800

    0613

--- 378 Kth Smallest Element in a Sorted Matrix.java
+++ 378 Kth Smallest Element in a Sorted Matrix.java
@@ -49,3 +49,56 @@ public class Solution {
         
     }
 }
+
+
+/* my solution
+class Pair {
+    public int x;
+    public int y;
+    public int val;
+    
+    public Pair(int x, int y, int val) {
+        this.x = x;
+        this.y = y;
+        this.val = val;
+    }
+}
+
+class PairComparator implements Comparator<Pair> {
+    public int compare(Pair a, Pair b) {
+        return a.val - b.val;
+    }
+}
+
+public class Solution {
+
+    public int kthSmallest(int[][] matrix, int k) {
+        // write your code here
+        
+        int n = matrix.length;
+        int m = matrix[0].length;
+        boolean[][] used = new boolean[n][m];
+        
+        PriorityQueue<Pair> pq = new PriorityQueue<Pair>(k, new PairComparator()); //每次删掉一个，加入两个，会做k-1次
+        pq.add(new Pair(0, 0, matrix[0][0]));
+        used[0][0] = true;
+        
+        for(int i = 0; i < k - 1; i++) {
+            Pair cur = pq.poll();
+            int nextX = cur.x + 1;
+            int nextY = cur.y + 1;
+            if(nextX < n && !used[nextX][cur.y]) {
+                pq.add(new Pair(nextX, cur.y, matrix[nextX][cur.y]));
+                used[nextX][cur.y] = true;
+            }
+            if(nextY < m && !used[cur.x][nextY]) {
+                pq.add(new Pair(cur.x, nextY, matrix[cur.x][nextY]));
+                used[cur.x][nextY] = true;
+            }
+        }
+        
+        return pq.peek().val;
+        
+    }
+}
+*/
