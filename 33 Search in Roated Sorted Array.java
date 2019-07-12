e2345f6bcff30253bb9f91e160b6634efc3888da
sileyang <yangsile123@gmail.com>
2019-07-12 19:38:26 +0800

    0712

---
+++ 33 Search in Roated Sorted Array.java
@@ -0,0 +1,41 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        
+        if(nums.length == 0) {
+            return -1;
+        }
+        
+        int left = 0;
+        int right = nums.length - 1;
+        
+        while(left + 1 < right) {
+            int mid = left + (right - left) / 2;
+            
+            if(nums[mid] == target) {
+                return mid;
+            }
+            
+            if(nums[left] < nums[mid]) {
+                if(nums[mid] >= target && nums[left] <= target) {
+                    right = mid;
+                } else {
+                    left = mid;
+                }
+            } else {
+                if(nums[mid] <= target && nums[right] >= target) {
+                    left = mid;
+                } else {
+                    right = mid;
+                }
+            }
+        }
+        
+        if(nums[left] == target) {
+            return left;
+        } else if(nums[right] == target) {
+            return right;
+        }
+        
+        return -1;
+    }
+}
