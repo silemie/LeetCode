37f1adab647650df11817aa9313b45f00f49c850
sileyang <yangsile123@gmail.com>
2019-07-24 13:18:03 +0800

    0724

--- 110 Balanced Binary Tree.java
+++ 110 Balanced Binary Tree.java
@@ -39,3 +39,42 @@ public class Solution {
         return Math.max(getHeight(root.right), getHeight(root.left)) + 1;
     }
 }
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public boolean isBalanced(TreeNode root) {
+        if(root == null) {
+            return true;
+        }
+        
+        return balanceHelper(root) != -1;
+    }
+    
+    public int balanceHelper(TreeNode root) {
+        if(root == null) {
+            return 0;
+        }
+        
+        int left = balanceHelper(root.left);
+        int right = balanceHelper(root.right);
+        
+        if(left == -1 || right == -1) {
+            return -1;
+        }
+        
+        if(Math.abs(left - right) > 1) {
+            return -1;
+        }
+        
+        return Math.max(left, right) + 1;
+         
+    }
+}
