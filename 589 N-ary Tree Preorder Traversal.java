9885ba759c980a0a32cb1412e014d4e0f6d30bc7
sileyang <yangsile123@gmail.com>
2019-07-26 17:08:09 +0800

    0726

--- 589 N-ary Tree Preorder Traversal.java
+++ 589 N-ary Tree Preorder Traversal.java
@@ -34,3 +34,43 @@ class Solution {
         
     }
 }
+
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val,List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+class Solution {
+    
+    public List<Integer> preorder(Node root) {
+        List<Integer> ans = new ArrayList<>();
+        
+        
+        
+        preorderHelper(root, ans);
+        
+        return ans;
+    }
+    
+    private void preorderHelper(Node root, List<Integer> ans) {
+        
+        if(root == null) {
+            return;
+        }
+        
+        ans.add(root.val);
+        
+        for(Node n : root.children) {
+            preorderHelper(n, ans);
+        }
+    }
+}
